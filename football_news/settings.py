4e8805869ee32d173394df6e1ad99732750ddb19
farrelzzz <rajendramuhammadfarrel@gmail.com>
2025-08-27 14:30:27 +0700

    revisi nama URL

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-k9g&r&ps4ni#1&m_6vn9aaj4%tj-y1!a8k&xi&$p6u%h1ukd)$
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad.farrel46-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-farrel46-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
