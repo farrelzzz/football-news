b6cc15b2ee14717f0942dc7decf3d7573a4892f9
farrelzzz <rajendramuhammadfarrel@gmail.com>
2025-09-24 14:11:55 +0700

    selesai tutorial 4

--- main/views.py
+++ main/views.py
@@ -133,3 +133,8 @@ def edit_news(request, id):
     }
 
     return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
